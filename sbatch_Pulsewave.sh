1d527a3b232340386af0024f429d0e54211bef16
joaquinr-uner <jruiz@ingenieria.uner.edu.ar>
2022-04-29 16:45:30 -0300

    Corrected sbatch file

--- sbatch_Pulsewave.sh
+++ sbatch_Pulsewave.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #SBATCH --mail-user=jruiz@ingenieria.uner.edu.ar
-#SBATCH --partition=debug
+#SBATCH --partition=internos
 #SBATCH --nodes=1
 #SBATCH --ntasks=24
 #SBATCH --tasks-per-node=24
 #SBATCH --error=./job.%J.err
 #SBATCH --output=./job.%J.out
-#SBATCH --job-name=ElsqMN
-matlab -nodesktop -nodisplay -nosplash < E_lsq_Meei_Norm.m >out_EMN.txt 2>err_EMN.txt
+#SBATCH --job-name=Pulsew
+matlab -nodesktop -nodisplay -nosplash < ExpPulsewave.m >out_PW.txt 2>err_PW.txt
 
